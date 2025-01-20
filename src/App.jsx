7de49d424d14692f3bf506388a6916a8819e0e95
hande <hande.dann@gmail.com>
2025-01-20 22:45:31 +0300

    header component & google fonts & logo & title

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import "./App.css";
+import HomePage from "./pages/HomePage";
+import "./index.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header></Header>
       <p className="text-red-800 font-bold"> tailwind</p>
+      <HomePage></HomePage>
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { CiShoppingCart, CiSearch, CiUser } from "react-icons/ci";
+
+function Header() {
+  return (
+    <div>
+      <header className="flex justify-between h-16 items-center px-10 bg-stone-300">
+        <div className="flex gap-3 hover: cursor-pointer">
+          <img className="w-7" src="/src/images/icons/sofa-svgrepo-com.svg" />
+          <p className="logo text-xl">FURNITURE STORE.</p>
+        </div>
+        <nav>
+          <ul className="flex gap-8">
+            <li
+              className="hover: cursor-pointer hover:bg-stone-400 hover:rounded-lg hover: p-2"
+              href=""
+            >
+              Home
+            </li>
+            <li
+              className="hover: cursor-pointer hover:bg-stone-400 hover:rounded-lg hover: p-2"
+              href=""
+            >
+              Shop
+            </li>
+            <li
+              className="hover: cursor-pointer hover:bg-stone-400 hover:rounded-lg hover: p-2"
+              href=""
+            >
+              About Us
+            </li>
+            <li
+              className="hover: cursor-pointer hover:bg-stone-400 hover:rounded-lg hover: p-2"
+              href=""
+            >
+              Contact Us
+            </li>
+          </ul>
+        </nav>
+        <div className="flex text-2xl gap-2">
+          <i className="hover: cursor-pointer">
+            <CiShoppingCart />
+          </i>
+          <i className="hover: cursor-pointer">
+            <CiSearch />
+          </i>
+          <i className="hover: cursor-pointer">
+            <CiUser />
+          </i>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
